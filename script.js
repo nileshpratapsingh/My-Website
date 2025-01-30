bb4e3994382b0c1a7b10c5cff39c23d9fc41c759
Nilesh Pratap Singh <ns458458@gmail.com>
2025-01-31 00:43:49 +0530

    my first project website

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  function showLoginForm() {
+    document.getElementById("login-form").style.display = "block"; // Show login form
+    document.getElementById("signup-form").style.display = "none"; // Hide signup form
+  }
+
+  function showSignupForm() {
+    document.getElementById("login-form").style.display = "none"; // Hide login form
+    document.getElementById("signup-form").style.display = "block"; // Show signup form
+  }
+
